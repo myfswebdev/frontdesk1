b59abd8d61d1769fe71c6d2963a984aa8aa97b3c
myfswebdev <pradeepkrishnarao2023@gmail.com>
2024-06-14 22:23:58 +0530

    Create data.js

---
+++ pages/data.js
@@ -0,0 +1,6 @@
+export const data = [
+  { id: 1, name: "Item 1", parent: null },
+  { id: 2, name: "Item 2", parent: 1 },
+  { id: 3, name: "Item 3", parent: null },
+  // ... more data objects
+];
